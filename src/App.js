3501a33f0fa8519e6e05dd88e2bb541c1c7a1c8f
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-20 19:58:36 -0500

    add navbar and background color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Navbar from 'react-bootstrap/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/suits.png'
 
 
 class App extends Component {
@@ -8,9 +14,15 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<div>
+				<Navbar bg="dark" variant="dark">
+					<Navbar.Brand>
+						<img src={logo} width="30" height="30"/>{' '}
+						Single Player Casino
+					</Navbar.Brand>
+				</Navbar>
+				<Container>
 					App.js
-				</div>
+				</Container>
 			</div>
 		);
 	}
