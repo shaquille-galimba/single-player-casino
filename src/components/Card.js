e44c9c557a2e6d09cfad8f09016cfba6a35a0e0a
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2021-01-01 21:49:37 -0500

    change map method to forEach

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 function importAll(r) {
 	let images = {};
-  r.keys().map((item, index) => { return images[item.replace('./', '')] = r(item); });
+  r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
 	return images
 }
 
